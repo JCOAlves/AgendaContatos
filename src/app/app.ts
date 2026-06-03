94030ac41f7c55169bde30cbdbce6a105cbc0385
Júlio César <juliocesaroalves679@gmail.com>
2026-06-03 12:18:29 -0300

    feat: Adição de compornentes form e lista de contatos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaContatos } from '../lista-contatos/lista-contatos';
+import { FormContato } from '../form-contato/form-contato';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaContatos, FormContato],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('AgendaContatos');
+  protected readonly listaContatos = signal([{ nome: "Júlio", numero: "3534546" }]);
 }
---
+++ src/lista-contatos/lista-contatos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-contatos',
+  imports: [],
+  templateUrl: './lista-contatos.html',
+  styleUrl: './lista-contatos.css',
+})
+export class ListaContatos {}
